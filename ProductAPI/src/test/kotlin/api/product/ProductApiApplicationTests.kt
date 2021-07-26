ffc04e3f67c1b1f94bbd12c1594e7ac9edf1715e
Facu Mediotte <facumediotte@gmail.com>
2021-07-26 14:36:00 -0300

    Test de Integración - HTTP GET

--- ProductAPI/src/test/kotlin/api/product/ProductApiApplicationTests.kt
+++ ProductAPI/src/test/kotlin/api/product/ProductApiApplicationTests.kt
@@ -1,13 +1,24 @@
 package api.product
 
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ProductApiApplicationTests {
 
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
     @Test
-    fun contextLoads() {
+    fun findAll() {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/product"))
+            .andExpect(status().isOk)
     }
 
 }
