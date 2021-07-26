dc05b67f0bc77f590f23fb2a37861c9dad0debbe
Facu Mediotte <facumediotte@gmail.com>
2021-07-26 14:52:55 -0300

    Autoconfigure mockMvc Test capa controller contra capa de servicio

--- ProductAPI/src/test/kotlin/api/product/ProductApiApplicationTests.kt
+++ ProductAPI/src/test/kotlin/api/product/ProductApiApplicationTests.kt
@@ -1,5 +1,11 @@
 package api.product
 
+import api.product.domain.Product
+import api.product.service.ProductService
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
@@ -15,10 +21,23 @@ class ProductApiApplicationTests {
     @Autowired
     private lateinit var mockMvc: MockMvc
 
+    @Autowired
+    private lateinit var mapper: ObjectMapper
+
+    @Autowired
+    private lateinit var productService: ProductService
+
     @Test
     fun findAll() {
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/product"))
+        val productsFromService = productService.findAll()
+
+        val json = mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/product"))
             .andExpect(status().isOk)
+            .andReturn().response.contentAsString
+
+        val products: List<Product> = mapper.readValue(json)
+
+        assertThat(productsFromService, Matchers.`is`(Matchers.equalTo(products)))
     }
 
 }
