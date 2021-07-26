517d2ae81d9c5b521047572f96d15738e403dd43
Facu Mediotte <facumediotte@gmail.com>
2021-07-26 18:24:26 -0300

    Rest Exception Handlers

--- ProductAPI/src/main/kotlin/api/product/controller/ProductController.kt
+++ ProductAPI/src/main/kotlin/api/product/controller/ProductController.kt
@@ -2,9 +2,19 @@ package api.product.controller
 
 import api.product.domain.Product
 import api.product.service.ProductService
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.io.FileNotFoundException
 
 @RestController
 @RequestMapping("/api/v1/product")
-class ProductController(productService: ProductService) : BasicController<Product, String>(productService)
+class ProductController(
+    productService: ProductService
+) : BasicController<Product, String>(productService) {
+
+    @GetMapping("/test")
+    fun fileNotFoundTest() {
+        throw FileNotFoundException("just test")
+    }
+}
